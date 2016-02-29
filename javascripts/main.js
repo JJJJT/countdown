dbbf9d26de17fb293d50f22713abf87dfe1c0ad8
James Kong <k.kong@actimirror.com>
2016-02-29 13:11:16 +0800

    update scrip

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,12 @@
 function getTimeRemaining(endtime) {
   var t = Date.parse(endtime) - Date.parse(new Date());
-  var milliseconds = t ;
-  var seconds = Math.floor((t / 1000) % 60);
+
+  var d = new Date();
+  var now = new Date();
+  var n =  now.getMilliseconds()  - endtime.getMilliseconds() ;
+
+  var milliseconds = n ;
+  var seconds = t;//Math.floor((t / 1000) % 60);
   var minutes = Math.floor((t / 1000 / 60) % 60);
   var hours = Math.floor((t / (1000 * 60 * 60)) % 24);
   var days = Math.floor(t / (1000 * 60 * 60 * 24));
@@ -23,21 +28,21 @@ window.onload = function (){
 function initializeClock(id, endtime) {
   var clock = document.getElementById(id);
   console.log(clock);
-  var daysSpan = clock.querySelector('.days');
-  var hoursSpan = clock.querySelector('.hours');
-  var minutesSpan = clock.querySelector('.minutes');
+  // var daysSpan = clock.querySelector('.days');
+  // var hoursSpan = clock.querySelector('.hours');
+  // var minutesSpan = clock.querySelector('.minutes');
   var secondsSpan = clock.querySelector('.seconds');
-  var milliseconds = clock.querySelector('.milliseconds');
+  // var milliseconds = clock.querySelector('.milliseconds');
   
 
   function updateClock() {
     var t = getTimeRemaining(endtime);
 
-    daysSpan.innerHTML = t.days;
-    hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
-    minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
-    secondsSpan.innerHTML = ('0' + t.seconds).slice(-2);
-    milliseconds.innerHTML = ('0' + t.milliseconds);
+    // daysSpan.innerHTML = t.days;
+    // hoursSpan.innerHTML = ('0' + t.hours).slice(-2);
+    // minutesSpan.innerHTML = ('0' + t.minutes).slice(-2);
+    secondsSpan.innerHTML = ('0' + t.seconds).slice(-12,10);
+    // milliseconds.innerHTML = ('0' + t.milliseconds).slice(-3);
 
     if (t.total <= 0) {
       clearInterval(timeinterval);
@@ -45,7 +50,8 @@ function initializeClock(id, endtime) {
   }
 
   
-  var timeinterval = setInterval(updateClock, 10);
+  createjs.Ticker.timingMode = createjs.Ticker.RAF;
+  createjs.Ticker.addEventListener("tick", updateClock);
   
   updateClock();
 }
